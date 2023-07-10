ccef5e73b6c25c21e4e9d4cbd5be37cdb33a59b2
Rajkumar <114281193+rajkumarraghunathan@users.noreply.github.com>
2023-07-10 20:23:33 +0530

    commit

--- Route/auth.js
+++ Route/auth.js
@@ -6,9 +6,9 @@ exports.isAuth = async (req, res, next) => {
     // const { cookies } = req;
     // cookies.accessToken
     console.log(cookies);
-    if (cookies.accessToken) {
+    if (cookies) {
 
-        let user = jwt.verify(cookies.accessToken, process.env.SCERET_KEY)
+        let user = jwt.verify(cookies, process.env.SCERET_KEY)
 
         req.user = user;
         if (!req.user) {
